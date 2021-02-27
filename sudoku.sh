e07368f2d1e401f5468eea141ec82987fe75ea3a
Cory Washington <tbd>
2021-02-27 18:10:06 -0500

    Improve object model for the Sudoku board
    Improved efficiency object model (inefficiencies of old object model
    discussed in a previous commit msg). The new model uses a single array
    where each entry is a 2-char string "<value><type>" where
      <value> is the current entry value (digit or blank)
      <type>  is one of
        's' - starting entry value
        'g' - guessed  entry value
        ' ' - blank    entry value
    
    Each entry value and its type can be efficiently be looked up now.

--- sudoku.sh
+++ sudoku.sh
@@ -4,22 +4,23 @@
 # See `man bash` or `man bash-builtins` for more details (search for 'set')
 set -eu;
 
-# Initialize startingBoard array
+# Initialize board array
 # Testing board borrowed from: https://www.puzzle-sudoku.com/?e=MDozMCw2MTAsOTYx
-startingBoard=(
-  "115"                                     "187" "194"
-  "216" "221"                   "267"       "288"      
-              "338"             "363" "379"            
-        "425" "432"       "451"                        
-                    "548" "556" "564"                  
-                          "655"       "673" "681"      
-              "731" "749"             "774"            
-        "829"       "842"                   "886" "897"
-  "913" "928"                                     "992"
+board=(
+  "5s" "  " "  " "  " "  " "  " "  " "7s" "4s"
+  "6s" "1s" "  " "  " "  " "7s" "  " "8s" "  "
+  "  " "  " "8s" "  " "  " "3s" "9s" "  " "  "
+  "  " "5s" "2s" "  " "1s" "  " "  " "  " "  "
+  "  " "  " "  " "8s" "6s" "4s" "  " "  " "  "
+  "  " "  " "  " "  " "5s" "  " "3s" "1s" "  "
+  "  " "  " "1s" "9s" "  " "  " "4s" "  " "  "
+  "  " "9s" "  " "2s" "  " "  " "  " "6s" "7s"
+  "3s" "8s" "  " "  " "  " "  " "  " "  " "2s"
 )
-
-# Initialized guesses array
-guesses=( );
+# Types of entry
+START_ENTRY='s';
+GUESS_ENTRY='g';
+BLANK_ENTRY=' ';
 
 # Coloration
 export FAINT="$(tput dim)"
@@ -34,10 +35,26 @@ export GUESS_COLOR="${BRIGHT_YELLOW}"
 export BOARD_COLOR="${FAINT_GREEN}"
 
 # Helper functions to print parts of the board.
-# 'value' is the entry at the current coordinate.
-function echoStart() { echo -n "${START_COLOR}${value}${TPUT_RESET} "; }
-function echoGuess() { echo -n "${GUESS_COLOR}${value}${TPUT_RESET} "; }
-function echoBlank() { echo -n "  "; }
+# 'entry' is the entry at the current coordinate.
+function echoStartEntry() { echo -n "${START_COLOR}${entryValue}${TPUT_RESET} "; }
+function echoGuessEntry() { echo -n "${GUESS_COLOR}${entryValue}${TPUT_RESET} "; }
+function echoBlankEntry() { echo -n "  "; }
+function echoEntry() {
+  entryValue="${entry:0:1}";
+  entryType="${entry:1:1}";
+  case ${entryType} in
+    ${START_ENTRY})
+      echoStartEntry ;;
+    ${GUESS_ENTRY})
+      echoGuessEntry ;;
+    ${BLANK_ENTRY})
+      echoBlankEntry ;;
+    *)
+      echo "ERROR: invalid \${entry}: '${entry}'";
+      exit 1;
+    ;;
+  esac;
+}
 
 # Print a row seperator line if it is time for that.
 function echoSeperatorLine() {
@@ -57,32 +74,9 @@ function printBoard() {
     j=0;
     for j in {1..9}; do
       echoSeperatorBar;
-      coor="${i}${j}";
-      echoFunction="";
-      value="";
-      # If this coor is in the startingBoard, print it as such.
-      for entry in ${startingBoard[@]}; do
-        if [ ${entry:0:2} -eq ${coor} ]; then
-          value=${entry:2:1};
-          echoStart;
-          break;
-        fi;
-      done;
-      [ -n "${value}" ] && continue;
-
-      # If this coor has been guessed, print it as such.
-      # Ensure this doesn't happen if the entry is in the startingBoard (don't print twice!)
-      for entry in ${guesses[@]}; do
-        if [ ${entry:0:2} -eq ${coor} ]; then
-          value=${entry:2:1};
-          echoGuess;
-          break;
-        fi;
-      done;
-      [ -n "${value}" ] && continue;
-
-      # Print blank entry
-      echoBlank;
+      index=$(( 9 * (i-1) + (j-1) ));
+      entry=${board[${index}]};
+      echoEntry;
     done;
     # Print seperator lines
     j=10;
