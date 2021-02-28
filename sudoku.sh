dd2c4af32f29e3905b11a8f9095b2aebbc197c85
Cory Washington <tbd>
2021-02-27 20:20:06 -0500

    Add main logic to process user input till completion
    The Sudoku game is nominally playable now. User input is accepted and
    checked (at least nominally) for validity. The board is playable until
    all coordinates have a value; the validation does NOT currently check
    that the board follows the valid Sudoku solution rules.
    
    TODO:
      Improve input validation?
      Ensure input processing is safe (i.e., no system security issues).
      Perform a true check for game completion.
      Better reporting for invalid input.

--- sudoku.sh
+++ sudoku.sh
@@ -91,8 +91,79 @@ function printBoard() {
   done;
 }
 
+# Check whether the board is solved
+# INCOMPLETE: right now I'm just checking if there are any blank entries
+function checkBoardCompletion() {
+  solved=true;
+  for i in {1..9}; do
+    for j in {1..9}; do
+      computeIndex;
+      entry=${board[${index}]};
+      entryType="${entry:1:1}";
+      if [[ ${entryType} == ${BLANK_ENTRY_TYPE} ]]; then
+        solved=false;
+        return;
+      fi;
+    done;
+  done;
+}
+
 # Compute index from current ${i} and ${j} values
 function computeIndex() { index=$(( 9 * (i-1) + (j-1) )); }
 
+# Preprocess and validate the current `move`
+DIGIT_PATTERN="^[0-9]$";
+function preprocessAndValidateMove() {
+  validMove=true;
+  i=${move:0:1};
+  j=${move:1:1};
+  value=${move:2:1};
+  [[ ${i}     =~ ${DIGIT_PATTERN} ]] || { echo "invalid row"    && validMove=false; };
+  [[ ${j}     =~ ${DIGIT_PATTERN} ]] || { echo "invalid column" && validMove=false; };
+  [[ ${value} =~ ${DIGIT_PATTERN} ]] || { echo "invalid value"  && validMove=false; };
+}
+
+# Process the latest move
+function processMove() {
+  # TODO: maybe: use three inputs (i, j, value) on `read` rather than one input with post-processing
+
+  # Exit function if the move was invalid
+  preprocessAndValidateMove;
+  ${validMove} || return 0;
+
+  # Move was valid. Update the Sudoku board.
+  computeIndex;
+  newEntry="${value}${GUESS_ENTRY_TYPE}";
+  existingEntry=${board[${index}]};
+  existingEntryType=${existingEntry:1:1};
+  case ${existingEntryType} in
+    ${GUESS_ENTRY_TYPE} | ${BLANK_ENTRY_TYPE})
+      board[index]=${newEntry};
+      ;;
+    ${START_ENTRY_TYPE})
+      echo "INVALID MOVE - the selected coordinate was part of the starting board";
+      ;;
+    *)
+      echo "ERROR - somehow the current board has an invalid entry type at the selected coordinate";
+      exit 2;
+  esac;
+}
+
 # Main program
+prompt="Choose your next move. You may set a value on any blank space or overwrite any guess on a non-hint space.\
+Entries should be input as a 3-digit number.
+  The 1st digit is the row
+  The 2nd digit is the column
+  The 3rd digit is the value you want to set
+> "
+solved=false;
+while ! ${solved}; do
+  printBoard
+  read -p "${prompt}" move;
+  processMove;
+  checkBoardCompletion;
+  echo;
+done;
+
 printBoard
+echo "GAME COMPLETED!";
