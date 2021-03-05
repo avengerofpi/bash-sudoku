9d7eff4c404acf1b37273d76e5d9227afa9b51e0
Cory Washington <tbd>
2021-03-05 03:20:43 -0500

    Add formatting (highlighting) for invalid/duplicate entries
    Use a new Bash array to track when an entry needs invalid / (duplicated
    in its row/column/subsquare). Extra formatting is currently in the form
    of highlighting (background color), and the bg color is customized
    depending on whether the entry is a guess or is from the starting board.
    
    I have some echoDebug calls saved in this commit. Will remove them in
    the next commit.
    
    I start using the "local" keyword for some function variables in this
    commit. This is good practice that I should use more consistently, to
    avoid accidentally borrowing a value from the wrong context, and to be
    more explicit about which vars are local and which are global.

--- sudoku.sh
+++ sudoku.sh
@@ -25,6 +25,33 @@ board=(
   "3s" "8s" "  " "  " "  " "  " "  " "  " "2s"
 )
 
+# Initialize an array that will be used to flag bad entries,
+#   e.g., duplicate value in the entry's row, column, or subsquare
+# for modified print formatting.
+declare -a extraEntryFormatting;
+
+# Clear the extraEntryFormatting array
+function clearExtraEntryFormatting() {
+  for index in {0..80}; do
+    extraEntryFormatting[${index}]="";
+  done;
+}
+clearExtraEntryFormatting;
+
+# Set entryFlag for the current index. Any non-empty string will do.
+function setBadEntryFormatting() {
+  entry="${board[${index}]}";
+  entryType="${entry:1:1}";
+  case ${entryType} in
+    ${START_ENTRY_TYPE})
+      extraEntryFormatting[${index}]="${FLAGGED_START_HIGHLIGHT}";
+      ;;
+    ${GUESS_ENTRY_TYPE})
+      extraEntryFormatting[${index}]="${FLAGGED_GUESS_HIGHLIGHT}";
+      ;;
+  esac;
+}
+
 # Types of entry
 START_ENTRY_TYPE='s';
 GUESS_ENTRY_TYPE='g';
@@ -40,10 +67,14 @@ export BRIGHT_CYAN="${BRIGHT}$(tput setaf 14)"
 export BRIGHT_PURPLE="${BRIGHT}$(tput setaf 93)"
 export TPUT_RESET="$(tput sgr0)"
 
+export RED_BG="$(tput setab 9)"
+export GREEN_BG="$(tput setab 2)"
 export YELLOW_BG="$(tput setab 3)"
 
 export START_COLOR="${BRIGHT_RED}"
 export GUESS_COLOR="${BRIGHT_YELLOW}"
+export FLAGGED_START_HIGHLIGHT="${GREEN_BG}"
+export FLAGGED_GUESS_HIGHLIGHT="${RED_BG}"
 export BOARD_COLOR="${FAINT_GREEN}"
 
 export    DEBUG_COLOR="${BRIGHT_CYAN}"
@@ -54,8 +85,8 @@ export CRITICAL_COLOR="${YELLOW_BG}${BRIGHT_RED}"
 
 # Helper functions to print parts of the board.
 # 'entry' is the entry at the current coordinate.
-function echoStartEntry() { echo -n "${START_COLOR}${entryValue}${TPUT_RESET} "; }
-function echoGuessEntry() { echo -n "${GUESS_COLOR}${entryValue}${TPUT_RESET} "; }
+function echoStartEntry() { echo -n "${extraEntryFormatting[${index}]}${START_COLOR}${entryValue}${TPUT_RESET} "; }
+function echoGuessEntry() { echo -n "${extraEntryFormatting[${index}]}${GUESS_COLOR}${entryValue}${TPUT_RESET} "; }
 function echoBlankEntry() { echo -n "  "; }
 function echoEntry() {
   entryValue="${entry:0:1}";
@@ -116,19 +147,32 @@ function printBoard() {
 function checkRow() {
   validRow=true;
   # Use 'checkerStr' to track values that have been used (explain this more...)
-  checkerArray=( "."  "" "" ""  "" "" ""  "" "" "" );
+  local checkerArray=( "."  "" "" ""  "" "" ""  "" "" "" );
   for j in {1..9}; do
     computeIndex;
     entry=${board[${index}]};
     entryValue="${entry:0:1}";
-    checkerArray[${entryValue}]=".";
+    checkerArray[${entryValue}]="${checkerArray[${entryValue}]}${j}"; # append ${j}
   done;
   # Index v in checkerArray is set iff v=0 (ignored) or the
   # value 'v' occurs in row i at least once.
   for v in {1..9}; do
-    if [[ "${checkerArray[${v}]}" != "." ]]; then
+    local checkerStr="${checkerArray[${v}]}";
+echoDebug "v = ${v} @ index = ${checkerStr}";
+    if [[ "${checkerStr}" == "" ]]; then
+      # The value 'v' does not currently exist in this row
       validRow=false;
     else
+      # The value 'v' has occurred at least once in this row
+      local n=${#checkerStr};
+      if [ ${n} -gt 1 ]; then
+        # The value 'v' occurred more than once; flag the offending entries
+        for (( hitNumber=0 ; n - hitNumber ; hitNumber += 1 )); do
+          j=${checkerStr:${hitNumber}:1};
+          computeIndex;
+          setBadEntryFormatting;
+        done;
+      fi;
     fi;
   done;
 }
@@ -147,19 +191,32 @@ function checkRows() {
 function checkColumn() {
   validColumn=true;
   # Use 'checkerStr' to track values that have been used (explain this more...)
-  checkerArray=( "."  "" "" ""  "" "" ""  "" "" "" );
+  local checkerArray=( "."  "" "" ""  "" "" ""  "" "" "" );
   for i in {1..9}; do
     computeIndex;
     entry=${board[${index}]};
     entryValue="${entry:0:1}";
-    checkerArray[${entryValue}]=".";
+    checkerArray[${entryValue}]="${checkerArray[${entryValue}]}${i}"; # append ${i}
   done;
   # Index v in checkerArray is set iff v=0 (ignored) or the
   # value 'v' occurs in column j at least once.
   for v in {1..9}; do
-    if [[ "${checkerArray[${v}]}" != "." ]]; then
+    local checkerStr="${checkerArray[${v}]}";
+echoDebug "v = ${v} @ index = ${checkerStr}";
+    if [[ "${checkerStr}" == "" ]]; then
+      # The value 'v' does not currently exist in this row
       validColumn=false;
     else
+      # The value 'v' has occurred at least once in this column
+      local n=${#checkerStr};
+      if [ ${n} -gt 1 ]; then
+        # The value 'v' occurred more than once; flag the offending entries
+        for (( hitNumber=0 ; n - hitNumber ; hitNumber += 1 )); do
+          i=${checkerStr:${hitNumber}:1};
+          computeIndex;
+          setBadEntryFormatting;
+        done;
+      fi;
     fi;
   done;
 }
@@ -177,26 +234,47 @@ function checkColumns() {
 # Check subsquare (ii, jj)
 function checkSubSquare() {
   validSubSquare=true;
-  # Use 'checkerStr' to track values that have been used (explain this more...)
-  checkerArray=( "."  "" "" ""  "" "" ""  "" "" "" );
+  # Use 'checkerArrayI' and 'checkerArrayJ' to track values that have been used (explain this more...)
+  local checkerArrayI=( "."  "" "" ""  "" "" ""  "" "" "" );
+  local checkerArrayJ=( "."  "" "" ""  "" "" ""  "" "" "" );
   imin=$(( (ii - 1) * 3 + 1 ));
   jmin=$(( (jj - 1) * 3 + 1 ));
   imax=$(( imin + 3 ));
   jmax=$(( jmin + 3 ));
-  for (( i=imin ; i - imax ; i=i + 1 )); do
-    for (( j=jmin ; j - jmax ; j=j + 1 )); do
+  for (( i=imin ; i - imax ; i += 1 )); do
+    for (( j=jmin ; j - jmax ; j += 1 )); do
       computeIndex;
       entry=${board[${index}]};
       entryValue="${entry:0:1}";
-      checkerArray[${entryValue}]=".";
+      checkerArrayI[${entryValue}]="${checkerArrayI[${entryValue}]}${i}"; # append ${i}
+      checkerArrayJ[${entryValue}]="${checkerArrayJ[${entryValue}]}${j}"; # append ${j}
     done;
   done;
-  # Index v in checkerArray is set iff v=0 (ignored) or the
+  # Index v in checkerArrayI (or checkerArrayJ) is set iff v=0 (ignored) or the
   # value 'v' occurs in subsquare (ii, jj) at least once.
   for v in {1..9}; do
-    if [[ "${checkerArray[${v}]}" != "." ]]; then
+    local checkerStrI="${checkerArrayI[${v}]}";
+    local checkerStrJ="${checkerArrayJ[${v}]}";
+echoDebug "v = ${v}";
+echoDebug "  @ i_indexes = ${checkerStrI}";
+echoDebug "  @ j_indexes = ${checkerStrJ}";
+    # Just check one of checkerStrI or checkerStrJ (they should correspond)
+    if [[ "${checkerStrI}" == "" ]]; then
+      # The value 'v' does not currently exist in this subSquare
       validSubSquare=false;
     else
+      # The value 'v' has occurred at least once in this subSquare
+      # Just check one of checkerStrI or checkerStrJ (they should correspond)
+      local n=${#checkerStrI};
+      if [ ${n} -gt 1 ]; then
+        # The value 'v' occurred more than once; flag the offending entries
+        for (( hitNumber=0 ; n - hitNumber ; hitNumber += 1 )); do
+          i=${checkerStrI:${hitNumber}:1};
+          j=${checkerStrJ:${hitNumber}:1};
+          computeIndex;
+          setBadEntryFormatting;
+        done;
+      fi;
     fi;
   done;
 }
@@ -297,6 +375,7 @@ solved=false;
 echo "${instructions}";
 while ! ${solved}; do
   printBoard
+  clearExtraEntryFormatting;
   read -p "${prompt}" move;
   processMove;
   checkBoardCompletion;
